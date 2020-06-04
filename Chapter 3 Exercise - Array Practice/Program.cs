3d3fd8aa10ab27bbda58ed489c32f8f235140c6a
mtorres9095 <mtorres9095@gmail.com>
2020-06-03 20:30:52 -0400

    Add project files.

---
+++ Chapter 3 Exercise - Array Practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chapter_3_Exercise___Array_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 2, 3, 5, 8 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int numero = nums[i];
+                if (numero % 2 != 0)
+                {
+                    // Console.WriteLine(nums[i1]);
+                    Console.WriteLine(nums[i]);
+                }
+
+            }
+        }
+    }
+}
